e5a125b1dfb77dc6706dab7c7adc5885917dfcaf
Asthaviradiya <asthaviradiya114@gmail.com>
2024-06-12 08:01:40 +0530

    scrollable apps js added

--- script.js
+++ script.js
@@ -2,3 +2,20 @@ const navDialog = document.getElementById("nav-dialog");
 function handleMenu(){
     navDialog.classList.toggle('hidden');
 }
+function setupIntersectionObserver(element, isLTR, speed) {
+    const intersectionCallback = (entries) => {
+        const isIntersecting = entries[0].isIntersecting;
+        
+    }
+
+    const intersectionObserver = new IntersectionObserver(intersectionCallback);
+
+    intersectionObserver.observe(element);
+}
+const line1 = document.getElementById('line-1');
+const line2 = document.getElementById('line-2');
+const line3 = document.getElementById('line-3');
+
+setupIntersectionObserver(line1,true,0.15);
+setupIntersectionObserver(line2,false,0.15);
+setupIntersectionObserver(line3,true,0.15);
