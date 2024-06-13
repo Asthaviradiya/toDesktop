c36ccf122a7c936a0d0c5232352200b70a909695
Asthaviradiya <asthaviradiya114@gmail.com>
2024-06-13 07:07:35 +0530

    app scroll is added

--- script.js
+++ script.js
@@ -2,15 +2,34 @@ const navDialog = document.getElementById("nav-dialog");
 function handleMenu(){
     navDialog.classList.toggle('hidden');
 }
+const initLTR = -48*4;
+const initRTL = 36*4;
 function setupIntersectionObserver(element, isLTR, speed) {
     const intersectionCallback = (entries) => {
         const isIntersecting = entries[0].isIntersecting;
-        
+        if(isIntersecting){
+            document.addEventListener('scroll', scrollHandler);
+        }
+        else{
+            document.removeEventListener('scroll',scrollHandler);
+        }
     }
 
     const intersectionObserver = new IntersectionObserver(intersectionCallback);
 
     intersectionObserver.observe(element);
+    function scrollHandler(){
+        
+        const translateX = (window.innerHeight-element.getBoundingClientRect().top)*speed;
+        let totaltanslate = 0;
+        if(isLTR){
+            totaltanslate = translateX+initLTR
+        }
+        else{
+            totaltanslate = -(translateX+initRTL)
+        }
+        element.style.transform = `translateX(${totaltanslate}px)`;
+    }
 }
 const line1 = document.getElementById('line-1');
 const line2 = document.getElementById('line-2');
